c661e7679bb3dd11ca95da1052e1ff87f7db4ff7
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2023-08-17 03:29:24 +0100

    new commit

--- login.js
+++ login.js
@@ -1,18 +1,19 @@
-function copy() {
-  // get the container
-  const element = document.querySelector("#example1");
-  // Create a fake `textarea` and set the contents to the text
-  // you want to copy
-  const storage = document.createElement("textarea");
-  storage.value = element.innerHTML;
-  element.appendChild(storage);
+const email_input_android = document.querySelector("#password_input");
+const close_button_android = document.querySelector("#close_button");
+const footer_android = document.querySelector("#footer");
+const show_password_android = document.querySelector("#show_password_android"); //
 
-  // Copy the text in the fake `textarea` and remove the `textarea`
-  storage.select();
-  storage.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-  element.removeChild(storage);
-}
-document.querySelector("#close_button").addEventListener("click", () => {
-  document.querySelector("#footer").style.display = "none";
+////////////////////////////////////////////////
+
+show_password_android.addEventListener("click", () => {
+  if (email_input_android.type === "password") {
+    email_input_android.type = "text";
+    show_password_android.innerText = "Hide";
+  } else {
+    email_input_android.type = "password";
+    show_password_android.innerText = "Show";
+  }
+});
+close_button.addEventListener("click", () => {
+  footer.style.display = "none";
 });
--- sign.js
+++ sign.js
@@ -1,5 +1,8 @@
-document
-  .querySelector("#close_icon_button_android")
-  .addEventListener("click", () => {
-    document.querySelector(".download_coontainer").style.display = "none";
-  });
+const close_icon_button_android = document.querySelector(
+  "#close_icon_button_android"
+);
+const download_containers = document.querySelector(".download_containers");
+///////////////////////////////////////////////////////////////////////////
+close_icon_button_android.addEventListener("click", () => {
+  download_containers.style.display = "none";
+});
