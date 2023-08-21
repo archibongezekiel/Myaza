b96c666eeb6407561fbf06bfbba1d43dc6503d7b
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2023-08-21 01:56:13 +0100

    new commit

--- sign.js
+++ sign.js
@@ -2,10 +2,16 @@ const close_icon_button_android = document.querySelector(
   "#close_icon_button_android"
 );
 const download_containers = document.querySelector(".download_containers");
+const xAndroid = document.querySelector(".check_box_container_android");
+
+const x = document.querySelector(".check_box_container");
+const z = document.querySelector("#check_icon");
+const zAndroid = document.querySelector("#check_icon_android");
 /////////////////////////////////////////////////////////////////////
+x.addEventListener("click", () => {
+  Checkbox();
+});
 function Checkbox() {
-  var x = document.querySelector(".check_box_container");
-  var z = document.querySelector("#check_icon");
   if (x.style.backgroundColor === "white") {
     x.style.backgroundColor = "#5645f5";
     z.style.display = "block";
@@ -16,19 +22,23 @@ function Checkbox() {
 }
 ///////////////////////// Android///////////////////////
 /////////////////////////////////////////////////////////////////////////////////
+xAndroid.addEventListener("click", () => {
+  checkBoxAndroid();
+});
+//////////////////////////////////////////////
 function checkBoxAndroid() {
-  var x = document.querySelector(".check_box_container_android");
-  var z = document.querySelector("#check_icon_android");
-  if (x.style.backgroundColor === "white") {
-    x.style.backgroundColor = "#5645f5";
-    z.style.display = "block";
+  if (xAndroid.style.backgroundColor === "white") {
+    xAndroid.style.backgroundColor = "#5645f5";
+    zAndroid.style.display = "block";
   } else {
-    x.style.backgroundColor = "white";
-    z.style.display = "none";
+    xAndroid.style.backgroundColor = "white";
+    zAndroid.style.display = "none";
   }
 }
+
 ///////////////////////////////////////////////////////////////////////////
 close_icon_button_android.addEventListener("click", () => {
   download_containers.style.display = "none";
 });
+
 ////////////////////////
