f7c12b7e1ed62889719ef5b22da28297ada14a5d
Ezekiel Archibong <112735477+ezekielarchibong@users.noreply.github.com>
2023-08-21 01:41:02 +0100

    new commit

--- sign.js
+++ sign.js
@@ -2,7 +2,31 @@ const close_icon_button_android = document.querySelector(
   "#close_icon_button_android"
 );
 const download_containers = document.querySelector(".download_containers");
+
 ///////////////////////////////////////////////////////////////////////////
 close_icon_button_android.addEventListener("click", () => {
   download_containers.style.display = "none";
 });
+////////////////////////
+function Checkbox() {
+  var x = document.querySelector(".check_box_container");
+  var z = document.querySelector("#check_icon");
+  if (x.style.backgroundColor === "white") {
+    x.style.backgroundColor = "#5645f5";
+    z.style.display = "block";
+  } else {
+    x.style.backgroundColor = "white";
+    z.style.display = "none";
+  }
+}
+function checkBoxAndroid() {
+  var x = document.querySelector(".check_box_container_android");
+  var z = document.querySelector("#check_icon_android");
+  if (x.style.backgroundColor === "white") {
+    x.style.backgroundColor = "#5645f5";
+    z.style.display = "block";
+  } else {
+    x.style.backgroundColor = "white";
+    z.style.display = "none";
+  }
+}
